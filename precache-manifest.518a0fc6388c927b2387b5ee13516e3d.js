a257c64be1e981dd7c7751ff50335676a3c0f9d5
niv b <nivb52@gmail.com>
2020-01-08 00:12:01 +0200

    Updates

---
+++ precache-manifest.518a0fc6388c927b2387b5ee13516e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df85bedf3cfb546f678efe9bab098654",
+    "url": "/mylocations/index.html"
+  },
+  {
+    "revision": "e6584af7d96f6cbb7f8e",
+    "url": "/mylocations/static/css/main.c00ab3bc.chunk.css"
+  },
+  {
+    "revision": "4540eab8a9239550d3a2",
+    "url": "/mylocations/static/js/2.ff01f442.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mylocations/static/js/2.ff01f442.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6584af7d96f6cbb7f8e",
+    "url": "/mylocations/static/js/main.e9d79a86.chunk.js"
+  },
+  {
+    "revision": "896f09188795f8006907",
+    "url": "/mylocations/static/js/runtime-main.24bdee7c.js"
+  }
+]);
