b4b22a511f7ea67191ee2a38dc4719d838c152be
niv b <nivb52@gmail.com>
2020-01-07 20:21:01 +0200

    delete css and add some cmps

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Categories from "./views/Categories";
+import "./style/App.css";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Categories />
+    </div>
+  );
+}
+
+export default App;
