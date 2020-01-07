cad4151889dd20be3350ca654e7ad0da1560f2e8
niv b <nivb52@gmail.com>
2020-01-07 20:21:22 +0200

    add util service

---
+++ src/services/UtilServices.js
@@ -0,0 +1,9 @@
+
+export function makeId(length = 7) {
+    let txt = ''
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return txt
+}
