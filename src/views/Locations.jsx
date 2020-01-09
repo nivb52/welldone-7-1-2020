de893258aaaa9507b5cd72ef6b6be9a878ba9260
niv b <nivb52@gmail.com>
2020-01-09 20:31:46 +0200

    control for child card inputs + select options

--- src/views/Locations.jsx
+++ src/views/Locations.jsx
@@ -2,58 +2,87 @@ import React, { useEffect, useState } from "react";
 import Cards from "../cmps/common/Cards";
 import locService from "../services/LocService";
 import catService from "../services/CatsService";
+import Select from "../cmps/common/Select";
 
 // TODO :
 // .. LOCATION properties: name, address, coordinates, and category.
 // show location on the map
-// ADD LOCATION -> must associate with category
-// manage location same as categories
 
-export default function Locations() {
+export default function Locations({ isCategorryChanged }) {
+  const fieldsToView = ["name", "address", "category"];
+  const fieldsToEdit = ["name", "address"];
+
   const [categories, setCategories] = useState([
     { id: null, name: "loading..." }
   ]);
   const [locs, setLocs] = useState([]);
+  const [isAsec, setIsAsec] = useState(true);
+  const [selectCategoryOption, setSelectCategoryOption] = useState();
+  const [filteredBy, setFilteredBy] = useState("all");
 
-  useEffect(async () => {
-    getLocations();
-    setCategories(await catService.getCat());
+  useEffect(() => {
+      getLocations();
   }, []);
 
-  //  let isAjaxing = false;
+  useEffect(() => {
+    getCategories();
+  }, [isCategorryChanged]);
 
+  // service
   const getLocations = async () => {
     setLocs(await locService.getLoc());
     return locs;
   };
 
+  // service
+  const getCategories = async () => {
+    const cats = await catService.getCat();
+    setCategories(cats);
+  };
+
+  // service
   const deleteLocation = async id => {
     await locService.delLoc(id);
     getLocations();
   };
 
-  const editOrAddLocation = async editedCat => {
-    await locService.editOrAdd(editedCat);
+  // service
+  const editOrAddLocation = async editedLoc => {
+    const newLoc = { ...editedLoc, category: selectCategoryOption };
+    await locService.editOrAdd(newLoc);
     getLocations();
   };
 
+  // service
   const filterBy = e => {
     const { value } = e.target;
-    if (value === 'all') getLocations()
+    setFilteredBy(value);
     const filteredDb = locService.filterBy(value, "category");
     setLocs(filteredDb);
   };
 
+  // service
   const groupBy = () => {
     locService.sortBy("category");
     getLocations();
   };
 
-  const [isAsec, setIsAsec] = useState(true);
+  // service
   const sortBy = () => {
     locService.sortBy("name", isAsec);
     setIsAsec(!isAsec);
     getLocations();
+  };
+
+  const currentEdit = editCard => {
+    // set currect option for the following html select options
+    setSelectCategoryOption(editCard.category);
+  };
+
+  const changeLocCategory = e => {
+    const { value } = e.target;
+    // set the select
+    setSelectCategoryOption(value);
   };
 
   return (
@@ -62,18 +91,20 @@ export default function Locations() {
         {categories && categories[0] && (
           <div>
             filter
-            <select className="btn" onChange={filterBy}>
-            <option key='all' value='all'> All </option>
-              {categories.map(category => (
-                <option key={category.id} value={category.name}>
-                  {category.name}
-                </option>
-              ))}
-            </select>
+            <Select
+              title=""
+              dataList={categories}
+              onSelect={filterBy}
+              selectedOption={filteredBy}
+              valueKey={"name"}
+              showKey={"name"}
+              optionClass="capitalized"
+              getSelectList={getCategories}
+            ></Select>
           </div>
         )}
         <button className="btn" onClick={sortBy}>
-          sort {isAsec ? " a - z" : " z    - a"}
+          sort {isAsec ? "  a - z" : "  z - a"}
         </button>
         <button className="btn" onClick={groupBy}>
           group by category
@@ -84,7 +115,24 @@ export default function Locations() {
         deleteCard={deleteLocation}
         editOrAddCards={editOrAddLocation}
         title="Locations"
-      ></Cards>
+        inputs={fieldsToView}
+        editInputs={fieldsToEdit}
+        classInput="mr-bottom-1rem"
+        currentEdit={currentEdit}
+        showMap={true}
+      >
+        <Select
+          title="category"
+          isShowAllOption={false}
+          selectedOption={selectCategoryOption}
+          dataList={categories}
+          onSelect={changeLocCategory}
+          valueKey={"name"}
+          showKey={"name"}
+          optionClass="capitalized"
+          getSelectList={getCategories}
+        ></Select>
+      </Cards>
     </div>
   );
 }
