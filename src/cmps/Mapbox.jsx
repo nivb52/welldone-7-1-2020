5936556580c77af6f59399191a1c5b1df5f10b38
niv b <nivb52@gmail.com>
2020-01-09 21:11:01 +0200

    add map with coords and api

--- src/cmps/Mapbox.jsx
+++ src/cmps/Mapbox.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+const api = require('../secrets/api.json');
 
-export default function Mapbox() {
+export default function Mapbox({coords}) {
   const Map = ReactMapboxGl({
     accessToken:
-      "pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A"
+        api.mapbox
   });
-  // in render()
   return (
     <Map
       style="mapbox://styles/mapbox/streets-v9"
       containerStyle={{
-        height: "100vh",
-        width: "100vw"
+        height: "50vh",
+        width: "50%",
+        transform:"translate3d(50%,0,0)"
       }}
     >
       <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
-        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        <Feature coordinates={coords} />
       </Layer>
     </Map>
   );
