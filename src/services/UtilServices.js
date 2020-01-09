ebb996c939aaea702ce5fb39bb6c544cffed5a78
niv b <nivb52@gmail.com>
2020-01-09 20:28:29 +0200

    add compare string functio

--- src/services/UtilServices.js
+++ src/services/UtilServices.js
@@ -1,9 +1,15 @@
-
 export function makeId(length = 7) {
-    let txt = ''
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    for (var i = 0; i < length; i++) {
-        txt += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-    return txt
+  let txt = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return txt;
+}
+
+export function compareString(a, b, key) {
+  if (a[key] < b[key])  return -1;
+  if (a[key] > b[key]) return 1;
+  return 0;
 }
