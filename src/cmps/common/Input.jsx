7d5e20a768e86c01627b82c6c4cb50e8dbb7273f
niv b <nivb52@gmail.com>
2020-01-09 20:22:25 +0200

    added onblur and simple validation mark

--- src/cmps/common/Input.jsx
+++ src/cmps/common/Input.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Input({ onEnter, placeholder, handleInput }) {
-
+export default function Input({
+  label = "name",
+  objectKey = '',
+  placeholder = "enter here",
+  onEnter,
+  handleInput,
+  handleBlur,
+  classInput = "",
+  defaultValue = ""
+}) {
+  const [userInput, setUserInput] = useState(defaultValue);
+  const [isValid, setIsValide] = useState(true);
   const handleEnter = e => {
     if (e.keyCode === 13) return onEnter();
   };
   const handleChange = e => {
     const { value } = e.target;
-    handleInput(value);
+    if (!value || !value.trim()) return setIsValide(false);
+    else {
+      setIsValide(true);
+      setUserInput(value);
+      handleInput(key,userInput)
+    }
   };
 
+  const key = objectKey || label
+
   return (
-    <input
-      autoFocus
-      onKeyDown={handleEnter}
-      placeholder={placeholder}
-      onChange={handleChange}
-    />
+    <div className="block">
+      <label className="block">{label}</label>
+      <input
+        className={classInput}
+        onKeyDown={handleEnter}
+        placeholder={placeholder}
+        onChange={handleChange}
+        onBlur={() => handleBlur(key, userInput)}
+      />
+      {isValid ? "✅" : "❌"}
+    </div>
   );
 }
