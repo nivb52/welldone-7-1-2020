38746f81352eec2c28d235d9ecfb4af207242011
niv b <nivb52@gmail.com>
2020-01-10 16:39:32 +0200

    re arrange and move to new folder

---
+++ src/cmps/crud/index.jsx
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+import Toolbar from "./Toolbar";
+import List from "../common/List";
+import Input from "../common/Input";
+import Mapbox from "../map";
+import "./cards.css";
+
+const Cards = ({
+  cards,
+  deleteCard,
+  editOrAddCards,
+  title = "Global Cards",
+  inputs = ["name"],
+  editInputs = ["name"],
+  classInput = "mr-bottom-1rem",
+  doOnSelect = () => {},
+  showMap,
+  children
+}) => {
+  const _findCard = id => {
+    let found = cards.find(c => c._id === id);
+    found = found === -1 ? null : found;
+    return found;
+  };
+
+  // #####################
+  // Invoke Parent Functions
+  const cardAdd = () => {
+    setEditCard({});
+    // will be added on 'handleBlur'
+  };
+
+  const cardDelete = id => {
+    deleteCard(id);
+    // emit to parent
+    unSelect();
+  };
+
+  // ==============
+  // handle on user Inputs
+  const handleSave = () => {
+    // emit to parent
+    editOrAddCards(editCard);
+    unSelect();
+  };
+  
+  const handleInput = (key, value) => {
+    setEditCard({ ...editCard, [key]: value });
+  };
+
+  const handleBlur = (key, value) => {
+    setEditCard({ ...editCard, [key]: value });
+  };
+
+  const onEnter = () => {
+    handleSave();
+  };
+  // end handle inputs
+  // ==============
+
+  // ###############
+  // Invoke Local Function (mostly, some invoke parent function)
+  const [selectCardId, setSelectCardId] = useState();
+  const [editCard, setEditCard] = useState();
+  const [viewCard, setViewCard] = useState();
+  
+  const handleCardChoose = item => {
+    // TODO : Check why i can see coords
+    if (item._id === selectCardId) unSelect();
+    else {
+      console.log('item ', item);
+      // emit to parent
+      doOnSelect(item);
+      setSelectCardId(item._id);
+    }
+  };
+
+  const cardEdit = id => {
+    const foundCard = _findCard(id);
+    setEditCard(foundCard);
+    // will be set as edited on 'handleBlur'
+  };
+
+  const cardView = id => {
+    const foundCard = _findCard(id);
+    setViewCard(foundCard);
+  };
+
+  const isHighlited = c => {
+    return selectCardId && c._id === selectCardId ? "highlight" : "";
+  };
+
+  const unSelect = () => {
+    setSelectCardId(null);
+    setEditCard();
+    setViewCard();
+    doOnSelect(null);
+  };
+
+  return (
+    <div className="cards">
+      {/* <!-- ================ ============== --> */}
+      {/* <!-- ========== Top Toolbar ============== --> */}
+      {/* <!-- ================ ============== --> */}
+      <Toolbar
+        id={selectCardId}
+        onEdit={editCard}
+        edit={cardEdit}
+        add={cardAdd}
+        view={cardView}
+        del={cardDelete}
+        back={unSelect}
+      >
+        {title}
+      </Toolbar>
+      {/* <!-- ================ ============== --> */}
+      {/* <!-- ========== LIST ============== --> */}
+      {/* <!-- ================ ============== --> */}
+      {cards && cards[0] && !editCard && !viewCard && (
+        <List
+          list={cards}
+          itemClass="capitalized"
+          handleClick={handleCardChoose}
+          classCondition={isHighlited}
+        />
+      )}
+      {/* <!-- ================ ============== --> */}
+      {/* <!-- =========== View Card ============== --> */}
+      {/* <!-- ================ ============== --> */}
+      {viewCard && (
+        <div className="view-card">
+          {inputs &&
+            inputs.map((field, i) => {
+              return (
+                <span key={i} className="capitalized mr-bottom-1rem block">
+                  {field + " : " + viewCard[field]}
+                </span>
+              );
+            })}
+          {viewCard && showMap && <Mapbox coords={viewCard.coords} />}
+        </div>
+      )}
+      {/* <!-- ================ ============== --> */}
+      {/* <!-- ========== Edit Card & New Card ============== --> */}
+      {/* <!-- ================ ============== --> */}
+      {editCard && (
+        <div className="edit-card capitalized">
+          {editInputs &&
+            editInputs[0] &&
+            editInputs.map((field, i) => {
+              return (
+                <Input
+                  key={i}
+                  label={field}
+                  classInput={classInput}
+                  onEnter={onEnter}
+                  placeholder={editCard[field] || field}
+                  defaultValue={editCard[field]}
+                  handleBlur={handleBlur}
+                  handleInput={handleInput}
+                />
+              );
+            })}
+          {children}
+          <button className="btn btn-save" onClick={handleSave}>
+            save
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cards;
