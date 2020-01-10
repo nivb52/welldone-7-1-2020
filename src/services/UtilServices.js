54e77839e68705269eb7fb4f5b266c77d07090ee
niv b <nivb52@gmail.com>
2020-01-10 16:39:59 +0200

    add debounce function

--- src/services/UtilServices.js
+++ src/services/UtilServices.js
@@ -8,6 +8,24 @@ export function makeId(length = 7) {
   return txt;
 }
 
+export function debounce (func, wait = 2000, immediate = null) {
+// If `immediate` is passed, trigger the function on the
+// leading edge, instead of the waiting.
+
+	let timeout;
+	return function() {
+		const context = this, args = arguments;
+		const later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 export function compareString(a, b, key) {
   if (a[key] < b[key])  return -1;
   if (a[key] > b[key]) return 1;
