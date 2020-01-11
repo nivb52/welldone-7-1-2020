74768917b7d0e5f9eba686721fa7d3cc99a8d5b4
niv b <nivb52@gmail.com>
2020-01-11 23:31:44 +0200

    obj nested property function

--- src/services/UtilServices.js
+++ src/services/UtilServices.js
@@ -8,26 +8,41 @@ export function makeId(length = 7) {
   return txt;
 }
 
-export function debounce (func, wait = 2000, immediate = null) {
-// If `immediate` is passed, trigger the function on the
-// leading edge, instead of the waiting.
-
-	let timeout;
-	return function() {
-		const context = this, args = arguments;
-		const later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		const callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
+export function hasOwnNestedProperty(obj,propertyPath){
+	// if (typeof obj !== 'object' && obj !== null) throw Error ('provided type is not a object or is a null')
+	const properties = propertyPath.split('.');
+	for (let i = 0; i < properties.length; i++ ){
+		const currKey = properties[i]
+		if (obj && obj.hasOwnProperty(currKey)) {
+			obj = obj[currKey]
+		} else return false
+	}
+	return true
+}
 
 export function compareString(a, b, key) {
-  if (a[key] < b[key])  return -1;
-  if (a[key] > b[key]) return 1;
-  return 0;
-}
+	if (a[key] < b[key])  return -1;
+	if (a[key] > b[key]) return 1;
+	return 0;
+  }
+  
+
+// // FROM UNDERSCORE
+// export function debounce (func, wait = 2000, immediate = null) {
+// // If `immediate` is passed, trigger the function on the
+// // leading edge, instead of the waiting.
+
+// 	let timeout;
+// 	return function() {
+// 		const context = this, args = arguments;
+// 		const later = function() {
+// 			timeout = null;
+// 			if (!immediate) func.apply(context, args);
+// 		};
+// 		const callNow = immediate && !timeout;
+// 		clearTimeout(timeout);
+// 		timeout = setTimeout(later, wait);
+// 		if (callNow) func.apply(context, args);
+// 	};
+// };
+
