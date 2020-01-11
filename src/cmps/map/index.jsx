166655ccf9ed536478ef68aeacbb4828959b9d5c
niv b <nivb52@gmail.com>
2020-01-11 23:32:00 +0200

    searcheable map & lazy load

--- src/cmps/map/index.jsx
+++ src/cmps/map/index.jsx
@@ -1,21 +1,30 @@
 import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import "./mapbox.css";
 
-import React, { useState } from "react";
-import MapGL, { GeolocateControl, Marker } from "react-map-gl";
+import React, { useState, Suspense } from "react";
+import MapGL, { Marker } from "react-map-gl";
+// Lazy load
+const GeolocateControl = React.lazy(() => import("react-map-gl"));
+const Geocoder = React.lazy(() => import("react-map-gl-geocoder"));
 
 const token = require("../../secrets/api.json");
-
 const TOKEN = token.mapbox;
 
+// Local vars
 const geolocateStyle = {
-  //   display: isEditable ? "block" : "none",
   float: "left",
   margin: "50px",
   padding: "10px"
 };
 
-const Map = ({ coords, editCoords = null, isEditable = false }) => {
+// .. The Map Component => 
+const Map = ({
+  coords,
+  editCoords = null,
+  editAddressByGeocoder = null,
+  isEditable = false
+}) => {
   const longitude = coords && coords[0] ? coords[0] : 0;
   const latitude = coords && coords[1] ? coords[1] : 0;
 
@@ -24,11 +33,26 @@ const Map = ({ coords, editCoords = null, isEditable = false }) => {
     height: "60vh",
     longitude,
     latitude,
-    zoom: 4
+    zoom: 5
   });
+  const mapRef = React.useRef();
+
+  const handleOnResult = ({ result }) => {
+    console.log(result);
+    const { coordinates } = result.geometry;
+    const [longitude, latitude] = coordinates;
+    setViewPort({ ...viewport, longitude, latitude, transitionDuration: 3000 });
+    // emit
+    if (editCoords && typeof editCoords === "function") {
+      editCoords(viewport);
+    }
+    if (editAddressByGeocoder && typeof editAddressByGeocoder === "function") {
+      editAddressByGeocoder(result);
+    }
+  };
 
   const _onViewportChange = viewport => {
-    if (!isEditable) return;
+    if (!isEditable) return ;
     setViewPort({ ...viewport, transitionDuration: 3000 });
     if (editCoords) editCoords(viewport);
   };
@@ -37,6 +61,7 @@ const Map = ({ coords, editCoords = null, isEditable = false }) => {
     <div style={{ display: "flex", justifyContent: "center" }}>
       <MapGL
         {...viewport}
+        ref={mapRef}
         mapboxApiAccessToken={TOKEN}
         mapStyle="mapbox://styles/mapbox/outdoors-v9"
         onViewportChange={_onViewportChange}
@@ -51,12 +76,22 @@ const Map = ({ coords, editCoords = null, isEditable = false }) => {
         </Marker>
 
         {isEditable && (
-          <GeolocateControl
-            style={geolocateStyle}
-            positionOptions={{ enableHighAccuracy: true }}
-            trackUserLocation={true}
-          />
-          
+          <>
+            <Suspense fallback={<div>Loading...</div>}>
+              <GeolocateControl
+                style={geolocateStyle}
+                positionOptions={{ enableHighAccuracy: true }}
+                trackUserLocation={true}
+                mapboxApiAccessToken={TOKEN}
+              />
+              <Geocoder
+                mapboxApiAccessToken={TOKEN}
+                mapRef={mapRef}
+                onResult={handleOnResult}
+                position="top-right"
+              />
+            </Suspense>
+          </>
         )}
       </MapGL>
       {!isEditable && (
